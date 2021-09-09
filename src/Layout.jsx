970cce12b3086414da5dd98e1e48861d0bb69e53
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-09-09 13:43:22 -0400

    Add basic layout

---
+++ src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <React.Fragment>
+      {/* header component */}
+      <h1>Header</h1>
+      {/* products list */}
+      <h2>Products List</h2>
+      {/* cart */}
+      <h2>Cart</h2>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
